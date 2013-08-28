2c5961a819825d6751313a13afa3c260b02c89db
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-08-28 08:32:46 +0200

    change indentation to use 4 spaces

--- test/exampleSpec.js
+++ test/exampleSpec.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
+
 describe('the program', function () {
-  it('can handle the truth', function () {
-    expect(true).to.equal(true);
-  });
+    it('can handle the truth', function () {
+        expect(true).to.equal(true);
+    });
 });
