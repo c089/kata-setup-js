146286ab12d3a45fdb3ac2f8796b251fb4fcd935
Christoph Neuroth <christoph.neuroth@gmail.com>
2017-02-02 20:12:01 +0100

    Remove sinon.js

--- test/spec.js
+++ test/spec.js
@@ -1,25 +1,9 @@
 var chai = require('chai'),
-    expect = chai.expect,
-    sinon = require('sinon'),
-    sinonChai = require('sinon-chai');
-chai.use(sinonChai);
+    expect = chai.expect;
 
 describe('the program', function () {
     it('can handle the truth', function () {
         expect(true).to.equal(true);
-    });
-
-    it('can spy on things', function () {
-        var spy = sinon.spy();
-        spy('hello');
-        expect(spy).to.have.been.calledOnce;
-        expect(spy).to.have.been.calledWith('hello');
-    });
-
-    it('can stub things', function () {
-      var stub = sinon.stub();
-      stub.returns(5);
-      expect(stub()).to.equal(5);
     });
 
 });
