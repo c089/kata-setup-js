f3fc1f2e7fbf82c99bdfa79a05519689c7d98cc2
Christoph Neuroth <christoph.neuroth@gmail.com>
2017-06-17 14:11:19 +0100

    SoCraTes UK 2017 async await kata

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,58 @@
-const {expect} = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
-describe('the program', function () {
-    it('can handle the truth', function () {
-        expect(true).to.equal(true);
+class CannotLocateUser extends Error {};
+class CannotFindCloseAirport extends Error{};
+
+const findNearestAirportUsingPromise = (locateUser, findAirportCloseTo) => {
+    return locateUser().then(findAirportCloseTo).catch(e => {
+        if (e instanceof CannotLocateUser) return 'LGW';
+        if (e instanceof CannotFindCloseAirport) throw e;
+    });
+};
+
+const findNearestAirportUsingAsyncAwait = async (locateUser, findAirportCloseTo) => {
+    try {
+        const location = await locateUser();
+        const airport = await findAirportCloseTo(location);
+
+        return airport;
+    }
+    catch (e) {
+        if (e instanceof CannotLocateUser) return 'LGW';
+        if (e instanceof CannotFindCloseAirport) throw e;
+    }
+};
+
+describe('findNearestAirport', function () {
+    const findNearestAirport = findNearestAirportUsingPromise;
+
+    it('given we cannot locate the user, assume they are in London', async () => {
+        const locateUser = async () => { throw new CannotLocateUser('Cannot locate User'); };
+        const findAirportCloseTo = () => {};
+
+        let nearestAirport = await findNearestAirport(locateUser, findAirportCloseTo);
+
+        expect(nearestAirport).to.equal('LGW');
     });
 
+    it('given the nearest airport cannot be located, abort', () => {
+        const locateUser = async () => { };
+        const findAirportCloseTo = async () => { throw new CannotFindCloseAirport; };
+        let nearestAirport = findNearestAirport(locateUser, findAirportCloseTo);
+
+        return expect(nearestAirport).to.be.rejected;
+    });
+
+    it('find the airports closest to the user', () => {
+        const locateUser = async () => 'Dorking';
+        const closestAirports = { Dorking: 'LGW' };
+        const findAirportCloseTo = async location => closestAirports[location];
+
+        const nearestAirport = findNearestAirport(locateUser, findAirportCloseTo);
+
+        return expect(nearestAirport).to.eventually.equal('LGW');
+    });
 });
